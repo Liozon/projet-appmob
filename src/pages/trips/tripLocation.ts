f77a07bd422f8c67b1c0279d1bb86dd30ae60785
Stefanie Schlumpf <stefanie.schlumpf@heig-vd.ch>
2019-01-22 09:29:59 +0100

    trying to fix problems

--- src/pages/trips/tripLocation.ts
+++ src/pages/trips/tripLocation.ts
@@ -48,5 +48,9 @@ export class TripLocationPage {
         });
     };
 
+    onMapReady(e) {
+        
+    }
+
 
 }
--- src/pages/trips/trips.ts
+++ src/pages/trips/trips.ts
@@ -6,6 +6,7 @@ import { TripPage } from './trip';
 import { NewTripPage } from './newTrip';
 import { Trip } from '../../models/trip';
 import { getSegmentsFromUrlPieces } from 'ionic-angular/umd/navigation/url-serializer';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'page-trips',
@@ -19,39 +20,43 @@ export class TripsPage {
 
   trips: any;
 
-  selectedTrip : Trip;
+  selectedTrip: Trip;
 
   constructor(private http: HttpClient, private rest: RestProvider, public navCtrl: NavController, public navParams: NavParams) {
+    this.getList();
     this.tripList = [];
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TripsPage');
     
+  }
+
+  getList() {
     this.rest.getTrips().subscribe(tripList => {
       this.tripList = tripList;
-      
     });
   }
 
-  onInput(e) {
+  onInput(e: any) {
+
+    //this.getList();
+
     // get the input-value
     var val = e.target.value;
-
-    if(!val) {
+    
+    if (!val) {
       return;
     }
 
     if (val && val.trim() != '') {
       this.tripList = this.tripList.filter((trip) => {
-        return trip.title.toLowerCase().indexOf(val.toLowerCase()) > -1 || trip.description.toLowerCase().indexOf(val.toLowerCase()) > -1;
+        return (trip.title.toLowerCase().indexOf(val.toLowerCase()) > -1 || trip.description.toLowerCase().indexOf(val.toLowerCase()) > -1);
       })
-    }
+    } 
   }
 
-  onCancel(e) {
-    e.clear();
-  }
+
 
   addTrip() {
     this.navCtrl.push(NewTripPage);
