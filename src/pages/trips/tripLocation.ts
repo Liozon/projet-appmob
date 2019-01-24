c673b9a68f814d29108ad67838da923083e41391
Stefanie Schlumpf <stefanie.schlumpf@heig-vd.ch>
2019-01-24 23:03:00 +0100

    fix problem of tripLocation

--- src/pages/trips/tripLocation.ts
+++ src/pages/trips/tripLocation.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation';
-import { MapOptions, tileLayer, latLng, Map, Marker } from 'leaflet';
+import { MapOptions, tileLayer, latLng, Map, Marker, marker } from 'leaflet';
 import { NavController, NavParams } from 'ionic-angular';
 import { Trip } from '../../models/trip';
 import { Place } from '../../models/place';
@@ -14,24 +14,21 @@ import { RestProvider } from '../../providers/rest/rest';
 export class TripLocationPage {
 
     trip: Trip;
+    placeList: Place[];
+
+    placeMarkers: Marker[];
+
     restProvider: RestProvider;
 
     mapOptions: MapOptions;
-    mapMarkers: Marker[];
+
+    initialLocation: any;
+    lat: any;
+    lng: any;
 
     map: Map;
-    places: Place[];
 
     constructor(public navCtrl: NavController, public navParams: NavParams, private geolocation: Geolocation, private rest: RestProvider) {
-        const tileLayerUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-        const tileLayerOptions = { maxZoom: 18 };
-        this.mapOptions = {
-            layers: [
-                tileLayer(tileLayerUrl, tileLayerOptions)
-            ],
-            zoom: 13,
-            center: latLng(46.778186, 6.641524)
-        };
     }
 
     ionViewDidLoad() {
@@ -39,18 +36,48 @@ export class TripLocationPage {
 
         this.trip = this.navParams.get("trip");
 
-        this.rest.getPlaces().subscribe(places => {
-            this.places = places;
-            this.mapMarkers = [];
-            places.forEach((place) => {
-                //this.mapMarkers = place.location.coordinates;
-            });
-        });
-    };
+        this.geolocation.getCurrentPosition().then(position => {
 
-    onMapReady(e) {
-        
+            this.initialLocation = latLng(position.coords.latitude, position.coords.longitude);
+            const tileLayerUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+            const tileLayerOptions = { maxZoom: 18 };
+            this.rest.getPlaces().subscribe(placeList => {
+                this.placeList = placeList;
+
+                this.placeMarkers = new Array();
+                placeList.forEach((place) => {
+                    if (place.tripId == this.trip.id) {
+                        this.lat = place.location.coordinates[1];
+                        this.lng = place.location.coordinates[0];
+                        this.placeMarkers.push(marker([this.lat, this.lng]).bindTooltip("Place : " + place.name));
+                    }
+                });
+                console.log(this.placeMarkers);
+            });
+
+            this.mapOptions = {
+                layers: [
+                    tileLayer(tileLayerUrl, tileLayerOptions)
+                ],
+                zoom: 5,
+                center: latLng(this.initialLocation)
+                //center: latLng()
+            };
+        }).catch(err => {
+            console.warn(`Could not retrieve user position because: ${err.message}`);
+        });
+
     }
 
+    onMapReady(map: Map) {
+        this.map = map;
+        /*
+        this.map.on('click', function (e) {
+            var coord = e.latlng;
+            var lat = coord.lat;
+            var lng = coord.lng;
+            console.log("You clicked the map at latitude: " + lat + " and longitude: " + lng);
+        });*/
+    }
 
 }
